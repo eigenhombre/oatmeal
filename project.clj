df67f23ecb12ce1900b00491d01a415f523ec2bd
John Jacobsen <eigenhombre@gmail.com>
2022-07-06 08:12:16 -0500

    Version 0.0.23

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject oatmeal "0.0.23-SNAPSHOT"
+(defproject oatmeal "0.0.23"
   :description "A tool for working with Common Lisp projects"
   :url "http://github.com/eigenhombre/oatmeal"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
