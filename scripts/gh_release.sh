00a1aace900299b5a4dc044bc07c9e4322a0fd1a
John Jacobsen <eigenhombre@gmail.com>
2022-07-05 21:23:14 -0500

    Moar testing releases

--- scripts/gh_release.sh
+++ scripts/gh_release.sh
@@ -7,6 +7,6 @@ echo "v$semver"
 
 gh release create v$semver --notes "$semver: scripted release"
 
-sha=$(curl https://github.com/eigenhombre/oatmeal/archive/refs/tags/v$semver.tar.gz | sha256sum)
-
+#sha=$(curl https://github.com/eigenhombre/oatmeal/archive/refs/tags/v$semver.tar.gz | sha256sum)
+sha=$(curl https://github.com/eigenhombre/oatmeal/archive/refs/tags/v$semver.tar.gz > /tmp/x; sha256sum x)
 echo "$semver : $sha"
